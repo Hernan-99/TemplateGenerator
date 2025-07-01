f48b97f016a528a975ef8d6a354c4cde5e98ea2c
Hernan-99 <hernan24744@gmail.com>
2025-06-30 21:50:02 -0300

    configuracion para deploy en vercel

--- Backend/src/config/origins.config.js
+++ Backend/src/config/origins.config.js
@@ -1,5 +1,5 @@
 const allowedOrigins = [
-  "https://sitio-web.com", // netlify
+  "https://templategenerator-frontend.netlify.app", // netlify
   "http://192.168.0.15:4200", // angular
   "http://localhost:4200", // angular
 ];
