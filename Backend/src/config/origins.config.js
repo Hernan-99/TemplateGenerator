b21e18d0674839d180846d12a1f787f220558f67
Hernan-99 <hernan24744@gmail.com>
2025-07-01 20:32:06 -0300

    cors origins

--- Backend/src/config/origins.config.js
+++ Backend/src/config/origins.config.js
@@ -1,6 +1,5 @@
 const allowedOrigins = [
   "https://templategenerator-frontend.netlify.app", // netlify
-  "https://template-generator-api.vercel.app/", // angular
   "http://localhost:4200", // angular
 ];
 
