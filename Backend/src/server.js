c845089c0f83785c4e0b4665d34c733c4792e62c
Hernan-99 <hernan24744@gmail.com>
2025-07-01 23:17:27 -0300

    sincronizacion de db y funcion autoinvocada para levantar el server

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -8,23 +8,7 @@ const verifyJWT = require("./middlewares/jwt.middleware.js");
 const credentials = require("./middlewares/credentials.js");
 
 const cookieParser = require("cookie-parser");
-
-// Importacion de Sequelize y modelos
-const sequelize = require("./config/db.config.js");
-const User = require("./models/user.model.js");
-const Template = require("./models/user.model.js");
-
-// Conexion y sincronizacion de tablas
-(async () => {
-  try {
-    await sequelize.authenticate();
-    console.log("✅ Conexión exitosa a la base de datos");
-    await sequelize.sync({ alter: true });
-    console.log("✅ Tablas sincronizadas");
-  } catch (error) {
-    console.log("❌ Error al conectar o sincronizar:", error);
-  }
-})();
+const initDB = require("./config/initDB.js");
 
 // Middlewares
 app.use(credentials);
@@ -44,7 +28,14 @@ app.use("/logout", require("./routes/logout.js"));
 app.use(verifyJWT);
 app.use("/templates", require("./routes/api/templates.js"));
 
-// Levantar servidor
-app.listen(PORT, () => {
-  console.log(`Servidor corriendo en el localhost:${PORT}`);
-});
+// conexion a la db y levantar servidor | IIFE
+(async () => {
+  try {
+    initDB();
+    app.listen(PORT, () => {
+      console.log(`Servidor corriendo en el localhost:${PORT}`);
+    });
+  } catch (err) {
+    console.error("❌ Falló la inicialización de la app:", err);
+  }
+})();
