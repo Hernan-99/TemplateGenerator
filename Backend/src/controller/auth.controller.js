1c4c5af61b543ff364a65ea3419c93c681398b6b
Hernan-99 <hernan24744@gmail.com>
2025-07-01 22:09:38 -0300

    refactorizacion de codigo adaptado a db

--- Backend/src/controller/auth.controller.js
+++ Backend/src/controller/auth.controller.js
@@ -1,38 +1,32 @@
-const path = require("node:path");
-const fsPromise = require("node:fs/promises");
+const User = require("../models/user.model.js");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
 
-const userModel = {
-  users: require("../models/users.json"),
-  setUsers: function (data) {
-    this.users = data;
-  },
-};
+// const path = require("node:path");
+// const fsPromise = require("node:fs/promises");
+// const userModel = {
+//   users: require("../models/users.json"),
+//   setUsers: function (data) {
+//     this.users = data;
+//   },
+// };
 
 const authLogin = async (req, res) => {
   const { email, password } = req.body;
   if (!email || !password)
     return res.status(400).json({ message: "Todos los campos son requeridos" });
 
-  const fs = require("fs");
-  userModel.users = JSON.parse(
-    fs.readFileSync(path.join(__dirname, "..", "models", "users.json"), "utf8")
-  );
+  try {
+    const findUser = User.findOne({ where: email });
+    if (!findUser) return res.sendStatus(401); // No autorizado
+    console.log("Usuario buscado:", findUser);
 
-  const findUser = userModel.users.find((user) => user.email === email);
-  if (!findUser) return res.sendStatus(401);
-  console.log("Usuario buscado:", findUser);
+    const matchPass = await bcrypt.compare(password, findUser.password);
+    if (!matchPass) return res.sendStatus(401); // No autorizado
+    console.log("Contraseña coincidente:", matchPass);
 
-  const matchPass = await bcrypt.compare(password, findUser.password);
-  console.log("Contraseña coincidente:", matchPass);
-
-  console.log("ACCESS_SECRET_TOKEN:", process.env.ACCESS_SECRET_TOKEN);
-
-  if (matchPass) {
-    const role = Object.values(findUser.role);
-
+    const role = findUser.role;
     const accessToken = jwt.sign(
       {
         UserData: {
@@ -45,35 +39,26 @@ const authLogin = async (req, res) => {
       // { expiresIn: "30s" }
     );
 
-
     const refreshToken = jwt.sign(
       { email: findUser.email },
       process.env.REFRESH_SECRET_TOKEN,
       { expiresIn: "1d" }
     );
 
-    const otherUsers = userModel.users.filter(
-      (user) => user.email !== findUser.email
-    );
+    // Actualiza el refreshToken en la db
+    await User.update({ refreshToken }, { where: { email: findUser.email } });
 
-    const currentUser = { ...findUser, refreshToken };
-
-    userModel.setUsers([...otherUsers, currentUser]);
-
-    await fsPromise.writeFile(
-      path.join(__dirname, "..", "models", "users.json"),
-      JSON.stringify(userModel.users)
-    );
-
+    // Enviar cookie httpOnly + accessToken
     res.cookie("jwt", refreshToken, {
       httpOnly: true,
       sameSite: "None",
       secure: true,
-      maxAge: 24 * 60 * 60 * 1000,
+      maxAge: 24 * 60 * 60 * 1000, // 1 día
     });
     res.json({ accessToken });
-  } else {
-    res.sendStatus(401);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Internal Server Error 500" });
   }
 };
 
