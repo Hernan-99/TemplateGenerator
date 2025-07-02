5bbea5fdd234925171d6598512f2e3cf168f040e
Hernan-99 <hernan24744@gmail.com>
2025-07-01 21:54:14 -0300

    refactorizacion de codigo adaptado a db

--- Backend/src/controller/register.controller.js
+++ Backend/src/controller/register.controller.js
@@ -1,46 +1,41 @@
-const fsPromise = require("node:fs/promises");
-const path = require("node:path");
+const User = require("../models/user.model.js");
 const bcrypt = require("bcrypt");
-
-const userModel = {
-  users: require("../models/users.json"),
-  setUsers: function (data) {
-    this.users = data;
-  },
-};
+// const fsPromise = require("node:fs/promises");
+// const path = require("node:path");
+// const userModel = {
+//   users: require("../models/users.json"),
+//   setUsers: function (data) {
+//     this.users = data;
+//   },
+// };
 
 const createUser = async (req, res) => {
   const { email, firstname, lastname, password } = req.body;
 
   if (!email || !firstname || !lastname || !password) {
-    return res.status(400).json({ message: "Todos los campos son requerido" });
+    return res.status(400).json({ message: "Todos los campos son requeridos" });
   }
 
-  const userExists = userModel.users.find((user) => user.email === email);
-  if (userExists) return res.sendStatus(409);
-
+  // Buscar si usuario ya existe
   try {
+    const userExists = await User.findOne({ where: { email } });
+    if (userExists)
+      return res.status(409).json({ message: "El usuario ya existe" });
+
+    // Hash contraseña
     const hashPass = await bcrypt.hash(password, 10);
-    const newUser = {
+    await User.create({
       email,
       firstname,
       lastname,
-      role: { "UserRol": 2001 },
       password: hashPass,
-    };
-    userModel.setUsers([...userModel.users, newUser]);
-    await fsPromise.writeFile(
-      path.join(__dirname, "..", "models", "users.json"),
-      JSON.stringify(userModel.users)
-    );
-    console.log(userModel.users);
-    res
-      .status(201)
-      .json({ success: `Usuario: ${email}. Creado correctamente` });
-  } catch (err) {
-    res
-      .status(500)
-      .json({ message: `Error al crear el usuario. Error: ${err.message}` });
+      role: { UserRol: 2001 },
+    });
+
+    res.status(201).json({ success: `Usuario ${email} creado correctamente` });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal Server Error 500" });
   }
 };
 
