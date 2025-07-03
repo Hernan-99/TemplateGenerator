9aaf4361a35312cd9801a9220cefe83185e364c3
Hernan-99 <hernan24744@gmail.com>
2025-07-03 11:28:12 -0300

    modificacion en la obtencion de roles

--- Backend/src/middlewares/role.middleware.js
+++ Backend/src/middlewares/role.middleware.js
@@ -3,11 +3,13 @@ const verifyRole = (...allowedRoles) => {
     if (!req?.role) return res.sendStatus(401);
 
     const roleArr = [...allowedRoles];
+    // verifico si recibe mas de un rol
+    const userRoles = Array.isArray(req.role) ? req.role : [req.role];
     console.log(
       `Tu rol [${!req.role}] no tiene permisos para realizar esta accion. Solo los roles: [${roleArr}] tienen acceso a esto.`
     );
 
-    const result = req.role
+    const result = userRoles
       .map((rol) => roleArr.includes(rol))
       .find((value) => value === true);
 
